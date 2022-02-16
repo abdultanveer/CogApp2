313266e35d1da68a2e86a0044ec14ba05c276cf6
abdul <abdul.tanveer@gmail.com>
2022-02-16 22:16:03 +0530

    asynctask -4 publishprogress & progressupdate

--- app/src/main/java/com/abdul/cogapp2/AsyncActivity.java
+++ app/src/main/java/com/abdul/cogapp2/AsyncActivity.java
@@ -4,17 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 
 public class AsyncActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
+        progressBar = findViewById(R.id.progressBar);
     }
 
     public void startDownload(View view) {
-        DownloadTask downloadTask = new DownloadTask();
+        DownloadTask downloadTask = new DownloadTask(progressBar);
         downloadTask.execute("http://youtube.com/movie");
     }
 }
--- app/src/main/java/com/abdul/cogapp2/DownloadTask.java
+++ app/src/main/java/com/abdul/cogapp2/DownloadTask.java
@@ -2,15 +2,30 @@ package com.abdul.cogapp2;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 //input type is string -- url, progress type - Integer,
 public class DownloadTask extends AsyncTask<String,Integer,Void> {
     public  static  String TAG =  DownloadTask.class.getSimpleName();
+
+    ProgressBar mProgressBar;
+    public DownloadTask(ProgressBar progressBar) {
+        mProgressBar = progressBar;
+    }
+
     //this method will execute on a seperate thread in the background
     //so i plan to download the movie in this method
     @Override
     protected Void doInBackground(String... url) {
-        Log.i(TAG,"downloading movie from the url"+ url[0]);
+        Log.i(TAG,"downloading movie from the url--"+ url[0]);
+        publishProgress(75);
         return null;
+    }
+
+    //this method gets called when there is an update[publishprogress]
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        mProgressBar.setProgress(values[0]);
     }
 }
