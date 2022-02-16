3b38c5f17df91409156b6c87e26f0619e6a943be
abdul <abdul.tanveer@gmail.com>
2022-02-16 22:07:06 +0530

    asynctask -2 asynctask

---
+++ app/src/main/java/com/abdul/cogapp2/DownloadTask.java
@@ -0,0 +1,7 @@
+package com.abdul.cogapp2;
+
+import android.os.AsyncTask;
+
+//input type is string -- url, progress type - Integer,
+public class DownloadTask extends AsyncTask<String,Integer,Void> {
+}
