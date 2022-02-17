c273280ab864bba0b737d8ff2037f9e6801ba977
abdul <abdul.tanveer@gmail.com>
2022-02-17 23:18:54 +0530

    firebase cloud messaging

--- app/src/main/java/com/abdul/cogapp2/AsyncActivity.java
+++ app/src/main/java/com/abdul/cogapp2/AsyncActivity.java
@@ -1,23 +1,50 @@
 package com.abdul.cogapp2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class AsyncActivity extends AppCompatActivity {
+    public static String TAG = AsyncActivity.class.getSimpleName();
     ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         progressBar = findViewById(R.id.progressBar);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(
+                new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                       // val token: String = task.getResult().toString()
+                       String token = task.getResult().toString();
+                        Log.i(TAG,"token is--"+token);
+
+                    }
+                }
+        );
+
     }
 
     public void startDownload(View view) {
@@ -27,13 +54,20 @@ public class AsyncActivity extends AppCompatActivity {
 
     public void showNotification(View view) {
         createNotificationChannel();
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "123")
+        Intent intent = new Intent(this, CalendarActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "123") // context and channel id are required
+                //all these below are optional
                 .setSmallIcon(R.drawable.ic_menu_camera)
                 .setContentTitle("Title notification")
                 .setContentText("content text longer text that cannot fit one line...")
                 .setStyle(new NotificationCompat.BigTextStyle()
                         .bigText("Much longer text that cannot fit one line..."))
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
         notificationManager.notify(007, builder.build());
