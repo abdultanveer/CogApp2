ad3dc280dc4f4645c4c9f34c5f3f15ab920ecb8b
abdul <abdul.tanveer@gmail.com>
2022-02-23 20:58:34 +0530

    RoomDb -- pojo-entity

---
+++ app/src/main/java/com/abdul/cogapp2/database/Todo.java
@@ -0,0 +1,15 @@
+package com.abdul.cogapp2.database;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Todo {
+
+    @PrimaryKey(autoGenerate = true)
+    public int tid;
+    
+    public String title;
+    public String notes;
+}
